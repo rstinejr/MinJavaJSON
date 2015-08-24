58395a1455c7e0c710cd6cf56614534505ccc8e9
Bob Stine <bstine@lgscout.com>
2015-08-24 09:29:40 -0400

    Make PipeWriter throwable.  Add function to return PipedOutputStream.

--- src/net/waltonstine/json/piped/PipeWriter.java
+++ src/net/waltonstine/json/piped/PipeWriter.java
@@ -1,8 +1,26 @@
 package net.waltonstine.json.piped;
 
-public class PipeWriter
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+
+public class PipeWriter implements Runnable
 {
-    public PipeWriter()
+    private String            jsonFile;
+    private PipedOutputStream strm;
+
+    public PipeWriter(String jsonFile) throws IOException
+    {
+        this.jsonFile = jsonFile;
+    }
+    
+    public PipedOutputStream openStream()
+    {
+        strm = new PipedOutputStream();
+        return strm;
+    }
+
+    public void run()
     {
     }
 }

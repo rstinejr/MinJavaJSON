03d15f7d3018f4df0409e7d9bac1e7669b392d95
Bob Stine <bstine@lgscout.com>
2015-08-24 12:50:11 -0400

    Stuf out PipeParser.  Will use it as sink from PipeWriter.

---
+++ src/net/waltonstine/json/piped/PipeParser.java
@@ -0,0 +1,82 @@
+package net.waltonstine.json.piped;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
+
+public class PipeParser
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		if (args.length != 1)
+		{
+		    System.err.printf("Need exactly one argument, the name of a JSON source file.\n");
+			System.exit(1);
+		}
+		String fname = args[0];
+		
+		InputStream fis = new FileInputStream(fname);
+
+		JsonParser jp = Json.createParser(fis);
+
+		/**
+		 * We can create JsonParser from JsonParserFactory also with below code
+		 * JsonParserFactory factory = Json.createParserFactory(null);
+		 * jp = factory.createParser(fis);
+		 */
+
+		System.out.printf("Parsinging file %s:\n", fname);
+
+		while (jp.hasNext()) 
+		{
+			Event event = jp.next();
+			System.out.printf("Event: %s\n", event.toString());
+			switch (event) 
+			{
+			case START_OBJECT:
+				System.out.printf("\nStart Object.\n");
+				break;
+			case END_OBJECT:
+				System.out.printf("End Object.\n");
+				break;
+			case START_ARRAY:
+				System.out.printf("Start Array.\n");
+				break;
+			case END_ARRAY:
+				System.out.printf("End Array.\n");
+				break;
+			case KEY_NAME:
+				System.out.printf("Key name '%s'\n", jp.getString());
+				break;
+			case VALUE_STRING:
+				System.out.printf("String value: '%s'\n", jp.getString());
+				break;
+			case VALUE_NUMBER:
+				System.out.printf("Number value: %d\n", jp.getLong());
+				break;
+			case VALUE_FALSE:
+				System.out.printf("Boolean value FALSE\n");
+				break;
+			case VALUE_TRUE:
+				System.out.printf("Boolean value TRUE\n");
+				break;
+			case VALUE_NULL:
+				System.out.printf("NULL value.\n");
+				break;
+			default:
+				// we are not looking for other events
+			}
+		}
+		
+		System.out.println("JSON parser done.");
+		
+		//close resources
+		fis.close();
+		jp.close();
+	}
+}
--- src/net/waltonstine/json/piped/PipeWriter.java
+++ src/net/waltonstine/json/piped/PipeWriter.java
@@ -1,26 +1,48 @@
 package net.waltonstine.json.piped;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.PipedOutputStream;
 
 public class PipeWriter implements Runnable
 {
-    private String            jsonFile;
-    private PipedOutputStream strm;
+    private FileInputStream   fstrm;
+    private PipedOutputStream pstrm;
 
-    public PipeWriter(String jsonFile) throws IOException
+    public PipeWriter()
     {
-        this.jsonFile = jsonFile;
     }
     
-    public PipedOutputStream openStream()
+    public PipedOutputStream openStream(String jsonFile) throws IOException
     {
-        strm = new PipedOutputStream();
-        return strm;
+        fstrm = new FileInputStream(jsonFile);
+        pstrm = new PipedOutputStream();
+
+        return pstrm;
     }
 
     public void run()
     {
+        final int BUFF_LN = 80;
+        byte[] readBuffer = new byte[BUFF_LN];
+
+        try
+        {
+            for ( ; ; )
+            {
+                int cnt = fstrm.read(readBuffer, 0, BUFF_LN);
+                if (cnt == -1)
+                {
+                    System.out.println("EOF on json file.");
+                    fstrm.close();
+                    break;
+                }
+            }
+        }
+        catch (IOException ioex)
+        {
+            System.err.printf("Exception reading json file: %s\n", ioex.toString());
+        }
     }
 }
